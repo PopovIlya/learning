8ef49a958d7675a9a6fb5afe5814d133a0dd6a69
PopovIlya <34451510+PopovIlya@users.noreply.github.com>
2017-12-13 19:16:20 +0300

    Add files via upload

---
+++ RTriangleSquare.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+public class rTriangle
+{
+    public double Main(double sideA, double sideB, double sideC)
+    {
+        double s;
+
+        try
+        {
+            // расчёт площади прямоугольного треугольника
+
+            if ((sideA > 0) & (sideB > 0) & (sideC > 0))  //проверка существования сторон
+            {
+                if ((sideA < sideB + sideC) & (sideB < sideA + sideC) & (sideC < sideA + sideB))
+                {       //проверка существования треугольника
+                    if (sideA == Math.Sqrt(sideB * sideB + sideC * sideC))
+                    {       //проверка: является ли треугольник прямоугольным, а сторона а гипотенузой?
+                        try
+                        {
+                            s = (sideB * sideC) / 2;        //вычисление площади прямоугольного треугольника
+                            return (s);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message); //если площадь треугольника слишком велика
+                            return (0);
+                        }
+                    }
+                    else if (sideB == Math.Sqrt(sideA * sideA + sideC * sideC))
+                    {      //проверка: является ли треугольник прямоугольным, а сторона b гипотенузой?
+                        try
+                        {
+                            s = (sideA * sideC) / 2;        //вычисление площади прямоугольного треугольника                      
+                            return (s);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message); //если площадь треугольника слишком велика
+                            return (0);
+                        }
+                    }
+                    else if (sideC == Math.Sqrt(sideA * sideA + sideB * sideB))
+                    {       //проверка: является ли треугольник прямоугольным, а сторона c гипотенузой?
+                        try
+                        {
+                            s = (sideA * sideB) / 2;        //вычисление площади прямоугольного треугольника                       
+                            return (s);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message); //если площадь треугольника слишком велика
+                            return (0);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введены данные не прямоугольного треугольника");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Треугольник с такими параметрами не существует, попробуйте ввести длины сторон ещё раз");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введены неверные значения сторон");
+            }
+            return (0);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return (0);
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestRightTriangle
+{
+    [TestClass]
+    public class RightTriangleTests
+    {
+
+        [TestMethod]
+        public void SquareTriangle() //правильно ли считает площадь
+        {
+
+            //arrange
+            var TestTriangle = new rTriangle();
+
+            //act == 6
+            var res = TestTriangle.Main(3, 4, 5);
+
+            //assert
+            Assert.AreEqual(6, res, "вычисления производятся верно");
+        }
+
+        //[TestMethod]
+        //public void HardData() //огроменный такой треугольник
+        //{
+
+        //    //arrange
+        //    var TestTriangle = new rTriangle();
+
+        //    //act == 6
+        //    var res = TestTriangle.Main(6000000000000000000, 6000000000000000000, 8485281374238570000);
+
+        //    //assert
+        //    Assert.AreEqual(0, res, "потому что Double, но программа всё равно показывает ответ, а тест не работает, почему?");
+        //}
+
+        //[TestMethod]
+        //public void TextData() //неверный формат вводимых данных
+        //{
+
+        //    //arrange
+        //    var TestTriangle = new rTriangle();
+
+        //    //act == 6
+        //    var res = TestTriangle.Main("три", "четыре", "пять");
+
+        //    //assert
+        //    Assert.AreEqual(0, res, "потому что текст не Double");
+        //}
+
+        [TestMethod]
+        public void NegativeData() //отрицательные переменные
+        {
+
+            //arrange
+            var TestTriangle = new rTriangle();
+
+            //act == 6
+            var res = TestTriangle.Main(-3, -4, -5);
+
+            //assert
+            Assert.AreEqual(0, res, "не существует треугольника с отрицательными сторонами");
+        }
+
+        [TestMethod]
+        public void squarefaketriangle()  // в случае несуществующего треугольника
+        {
+
+            //arrange
+            var TestTriangle = new rTriangle();
+
+            //act == 6
+            var res = TestTriangle.Main(40, 3, 8888);
+
+            //assert
+            Assert.AreEqual(0, res, "тест завершился без ошибки, т.к. любая из сторон треугольника меньше суммы двух других сторон и метод возвращает 0");
+        }
+
+        [TestMethod]
+        public void squarefakeRighttriangle()  // в случае не прямоугольного треугольника
+        {
+
+            //arrange
+            var TestTriangle = new rTriangle();
+
+            //act == 6
+            var res = TestTriangle.Main(4, 3, 3);
+
+            //assert
+            Assert.AreEqual(0, res, "тест завершился без ошибки, т.к. у прямоугольного треугольника квадрат гипотенузы равен сумме квадратов катетов и метод возвращает 0");
+        }
+    }
+}
